b82137a4edc8a816baa4d6f074d08a5d13397009
Thodler <neoskywalker01@gmail.com>
2024-11-06 10:00:28 +0100

    [UPD] Mise en place du script d'import avec mongo

--- init_import.sh
+++ init_import.sh
@@ -11,6 +11,15 @@ done
 echo "Cassandra est prêt, exécution du script CQL d'import."
 docker exec -i cassandra bash -c "cqlsh -f '/import/scripts/cassandra.cql'"
 
+# Attendre que MongoDB soit prêt
+echo "Attente de MongoDB..."
+until docker exec mongodb mongosh --eval "db.adminCommand('ping')"; do
+  sleep 5
+done
 
+echo "MongoDB est prêt, importation des données dans la collection 'immatriculations'."
+docker exec -i mongodb bash -c "mongoimport --db concessionnaire --collection immatriculations --file /data/import/Immatriculations.csv --type csv --headerline"
+
+echo "===== Initialisation terminée ====="
 
 sleep 10
