385fdb7350f92b78bcf2dfdc3299c695cbab8426
Laurent SINI <neoskywalker01@gmail.com>
2024-11-12 18:55:15 +0100

    [UPD] Optimisation

--- spark-apps/aggregate_catalogue_co2.py
+++ spark-apps/aggregate_catalogue_co2.py
@@ -1,22 +1,3 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import avg, when, trim, split, regexp_replace, round, lower
-
-spark = SparkSession.builder\
-    .appName("AggregateCatalogueCo2") \
-    .config('hive.metastore.warehouse.dir', '/user/hive/warehouse') \
-    .config('hive.metastore.uris', 'thrift://hive-metastore:9083') \
-    .enableHiveSupport()\
-    .getOrCreate()
-
-spark.sparkContext.setLogLevel("OFF")
-spark.catalog.clearCache()
-spark.sql("USE concessionnaire")
-
-df_catalogue = spark.sql("SELECT * FROM catalogue_ext")
-df_co2 = spark.sql("SELECT * FROM crit_air_ext")
-
-df_catalogue = df_catalogue.filter(df_catalogue['marque'] != 'marque')
-
 ##########
 # Nettoyage initial
 ##########
