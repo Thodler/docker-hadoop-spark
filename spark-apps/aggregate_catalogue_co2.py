34e9fc7b5adafc13843ae4ff99dcf9946de921f7
Thodler <neoskywalker01@gmail.com>
2024-11-12 15:51:14 +0100

    [UPD] Progression du script d'aggregation

--- spark-apps/aggregate_catalogue_co2.py
+++ spark-apps/aggregate_catalogue_co2.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import avg, when, trim, split, regexp_replace, round
+from pyspark.sql.functions import avg, when, trim, split, regexp_replace, round, lower
 
 spark = SparkSession.builder\
     .appName("AggregateCatalogueCo2") \
@@ -41,6 +41,12 @@ df_co2 = spark.sql("SELECT * FROM crit_air_ext")
 
 # Nettoyage initial
 df_catalogue = df_catalogue.filter(df_catalogue['marque'] != 'marque')
+df_catalogue = df_catalogue.withColumnRenamed("nom", "modele")
+
+df_catalogue = df_catalogue.withColumn("marque", lower(trim(col("marque")))) \
+                            .withColumn("modele", lower(trim(col("modele"))))
+
+df_co2 = df_co2.withColumn("marque_modele", lower(trim(col("marque_modele"))))
 
 # Traitement des colonnes avec nettoyage rigoureux
 df_co2 = df_co2.withColumn("bonus_malus", split(trim(df_co2["bonus_malus"]), "€").getItem(0))
@@ -52,12 +58,19 @@ df_co2 = df_co2.withColumn("rejets_co2", col("rejets_co2").cast("float"))
 
 # traitement de bonus_malus
 df_co2 = df_co2.withColumn("marque", split(df_co2["marque_modele"], " ", 2).getItem(0))
-df_co2 = df_co2.withColumn("model", split(df_co2["marque_modele"], " ", 2).getItem(1))
+df_co2 = df_co2.withColumn("modele", split(df_co2["marque_modele"], " ", 2).getItem(1))
 df_co2 = df_co2.drop('marque_modele')
 
-df_co2_agg = df_co2.groupby("marque").agg(round(avg("bonus_malus")).alias("moyenne_bonus_malus_marque"))
+df_co2_agg_bonus_malus = df_co2.groupby("marque").agg(round(avg("bonus_malus")).alias("moyenne_bonus_malus_marque"))
+df_co2_agg_rejets_co2 = df_co2.groupby("marque").agg(round(avg("rejets_co2")).alias("moyenne_rejets_co2_marque"))
+df_co2_agg_cout_energie = df_co2.groupby("marque").agg(round(avg("cout_energie")).alias("moyenne_cout_energie_marque"))
 
-df_co2 = df_co2.join(df_co2_agg, on="marque")
+df_co2 = df_co2.join(df_co2_agg_bonus_malus, on="marque")
+df_co2 = df_co2.join(df_co2_agg_rejets_co2, on="marque")
+df_co2 = df_co2.join(df_co2_agg_cout_energie, on="marque")
 
-# df_catalogue.show()
-df_co2.show()
+df_catalogue_with_moyennes = df_catalogue.join(df_co2, on=["marque"], how="left")
+# df_co2.show()
+df_catalogue.show()
+
+df_catalogue_with_moyennes.show(n=1000, truncate=False)
